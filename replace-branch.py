fc08732903c9e11e50efc30d2a9822c7ba2cd633
Marcin Glowacki <serine@o2.pl>
2018-01-13 16:56:33 +0000

    script

--- replace-branch.py
+++ replace-branch.py
@@ -1,1 +1,13 @@
-from git import REPO
+from git import Repo
+import sys
+
+repoPath = sys.argv[1]
+repoBranchName = sys.argv[2]
+repoRemoteName = sys.argv[3] if len(sys.argv) > 3 else 'origin'
+
+repo = Repo(repoPath)
+git = repo.git
+
+print git.status()
+print git.fetch('-v', repoRemoteName)
+print git.reset('--hard', '{0}/{1}'.format(repoRemoteName, repoBranchName))
