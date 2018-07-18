d9aced2e18821fcb944cb845c2a5e6fa78f20a39
Marcin Glowacki <mglowacki@bloomberg.net>
2018-07-18 19:41:45 +0100

    update to python and allow review

--- commit-wip.py
+++ commit-wip.py
@@ -3,26 +3,34 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='Git tools for pushing WIP changes to remote')
+parser.add_argument('--path', metavar='repo-path', default='.')
 parser.add_argument('-r', '--remote', metavar='name', default='origin')
-parser.add_argument('-n', '--no-push', dest='push', default='true', action='store_false')
+parser.add_argument('--review', dest='review', default=False, action='store_true')
+parser.add_argument('-n', '--no-push', dest='push', default=True, action='store_false')
 
 args = parser.parse_args(sys.argv[1:])
-print args
+print(args)
 
-repo = Repo('.')
+repo = Repo(args.path)
 msg = repo.head.reference.commit.message.strip()
 git = repo.git
+branch = repo.active_branch.name
+remoteBranch = branch + '-review' if args.review else branch
 
-print git.status()
-print git.add('.')
+print(git.status())
+print(git.add('.'))
 
-if msg == "WIP":
-    print 'amending WIP commit'
-    print git.commit('--amend', '--no-edit')
+if args.review:
+    print('amending previous commit')
+    print(git.commit('--amend', '--no-edit'))
 else:
-    print 'creating WIP commit'
-    print git.commit('-m', 'WIP')
+    if msg == "WIP":
+        print('amending WIP commit')
+        print(git.commit('--amend', '--no-edit'))
+    else:
+        print('creating WIP commit')
+        print(git.commit('-m', 'WIP'))
 
+print('pushing to remote \'{0}\', branch \'{1}\''.format(args.remote, remoteBranch))
 if args.push:
-    print 'pushing to remote \'{0}\''.format(args.remote)
-    git.push(args.remote, '--force')
+    print(git.push(args.remote, '{0}:{1}'.format(branch, remoteBranch), '--force'))
